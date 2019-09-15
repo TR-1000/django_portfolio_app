5503e3b51e845cd2c94b7225292ced0d139311f4
TR-1000 <t.l.ross@outlook.com>
2019-09-14 23:06:44 -0500

    Updated files

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -134,3 +134,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Heroku: Update database configuration from DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
--- personal_portfolio/wsgi.py
+++ personal_portfolio/wsgi.py
@@ -8,12 +8,10 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
+
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_portfolio.settings')
 
 application = get_wsgi_application()
-sys.path.append('/home/django_projects/MyProject')
-sys.path.append('/home/django_projects/MyProject/myproject')
