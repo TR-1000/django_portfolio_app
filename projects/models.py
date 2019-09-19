311842a003d60d16b3f09aee7de6f00f2fe70975
TR-1000 <t.l.ross@outlook.com>
2019-09-19 13:45:12 -0500

    Update for heroku

--- projects/models.py
+++ projects/models.py
@@ -7,3 +7,10 @@ class Project(models.Model):
     technology = models.CharField(max_length=110)
     image = models.URLField(max_length=1000, default="https://www.hdwallpapers.in/walls/windows_xp_bliss-wide.jpg")
     link = models.URLField(max_length=250, default='https://github.com/TR-1000')
+
+    def __str__(self):
+        return self.title
+        return self.description
+        return self.technology
+        return self.image
+        return self.link
