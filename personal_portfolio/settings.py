f01d84f9e41f1ec48b1ea938b3ed016902a11646
TR-1000 <t.l.ross@outlook.com>
2020-09-13 17:06:29 -0500

    changed settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("S_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tr-1000.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['tr-1000.herokuapp.com', 'localhost', 'tlross.info']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'projects',
     'blog',
+    'home',
 ]
 
 MIDDLEWARE = [
